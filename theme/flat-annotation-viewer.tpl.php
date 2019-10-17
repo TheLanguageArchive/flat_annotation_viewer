aac6de92a83cdf934dfcf17e72e092060e4b74aa
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2019-10-17 15:19:49 +0200

    new version annotation viewer

--- theme/flat-annotation-viewer.tpl.php
+++ theme/flat-annotation-viewer.tpl.php
@@ -1,1 +1,5 @@
-<annotation-viewer id="<?php echo $annotation_id; ?>"></annotation-viewer>
+<?php if (true === $accessible) : ?>
+    <annotation-viewer url="<?php echo $url; ?>" show-timestamps="<?php echo $show_timestamps == 1 ? 'true' : 'false'; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>"></annotation-viewer>
+<?php else : ?>
+    This file is not accessible to you.
+<?php endif; ?>
