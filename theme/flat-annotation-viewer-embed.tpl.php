f58bea31874247b0c3cd96a0e8d0105000398c27
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2020-05-20 14:40:37 +0200

    adding css

--- theme/flat-annotation-viewer-embed.tpl.php
+++ theme/flat-annotation-viewer-embed.tpl.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title;?></title>
+    <link rel="stylesheet" href="<?php echo $annotation_viewer_css; ?>"/>
 </head>
 <body>
 <?php if (true === $accessible) : ?>
